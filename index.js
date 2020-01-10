0b50a66e110d96e74bd1cc3fb8bbc10a182fe159
Dmitriy Sankevich <dmitriysankevich@tengizchevroil.com>
2020-01-10 16:45:03 +0500

    React Redux Tutorials - 11 - Middleware

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const redux = require('redux');
+const reduxLogger = require('redux-logger');
+
 const createStore = redux.createStore;
 const combineReducers = redux.combineReducers;
-
+const applyMiddleware = redux.applyMiddleware;
+const logger = reduxLogger.createLogger();
 
 const BUY_CAKE = 'BUY_CAKE';
 const BUY_ICECREAM = 'BUY_ICECREAM';
@@ -85,9 +88,9 @@ const rootReducer = combineReducers({
     cake: cakeReducer, 
     iceCream: iceCreamReducer
 });
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 console.log('initial state', store.getState());
-const unsubscribe = store.subscribe(() => console.log('Updated state', store.getState()));
+const unsubscribe = store.subscribe(() => {  });
 store.dispatch(buyCake());
 store.dispatch(buyCake());
 store.dispatch(buyCake());
