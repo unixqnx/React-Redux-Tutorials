4a1eb94e0101ed1bd9956029b7414b8fe35b6e1f
Dmitriy Sankevich <dmitriysankevich@tengizchevroil.com>
2020-01-10 14:41:39 +0500

    React Redux Tutorials - 6 - Reducers

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 // console.log('From index.js');
 const BUY_CAKE = 'BUY_CAKE'
 
-
-
-
-
 function buyCake(){
     return {
         type: BUY_CAKE,
         info: 'First redux action'
     }
 }
+
+//(previousStation, action) => newStation
+
+const initialState = {
+    numOfCakes: 10
+}
+
+const reducer = (state = initialState, action) => {
+    switch(action.type){
+        case BUY_CAKE: {
+            ...state,
+            numofCakes: state.numOfCakes-1
+        }
+
+        default: state
+    }
+}
+
