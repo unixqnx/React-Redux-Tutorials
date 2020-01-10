f7ebd023e8823aa30a95c5aebeea0487c2b094c6
Dmitriy Sankevich <dmitriysankevich@tengizchevroil.com>
2020-01-10 16:06:46 +0500

    React Redux Tutorials - 9 - Multiple Reducers

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const redux = require('redux');
 const createStore = redux.createStore;
 
-const BUY_CAKE = 'BUY_CAKE'
+const BUY_CAKE = 'BUY_CAKE';
+const BUY_ICECREAM = 'BUY_ICECREAM';
 
 function buyCake(){
     return {
@@ -10,34 +11,72 @@ function buyCake(){
     }
 }
 
+function buyIceCream(){
+    return {
+        type: BUY_ICECREAM
+    }
+}
+
+
+
 //(previousStation, action) => newStation
 
-const initialState = {
+// const initialState = {
+//     numOfCakes: 10,
+//     numOfIceCreams: 20
+// }
+
+
+const initialCakeState = {
     numOfCakes: 10
 }
 
-const reducer = (state = initialState, action) => {
+const initialIceCreamState = {
+    numOfIceCreams: 20
+}
+
+// const reducer = (state = initialState, action) => {
+//     switch(action.type){
+//         case BUY_CAKE: 
+//             let r ={};
+//             Object.assign(r, state);
+//             //...state,
+//             r.numOfCakes = r.numOfCakes - 1;
+//         return r;
+//         case BUY_ICECREAM: 
+//             let r2 ={};
+//             Object.assign(r2, state);
+//             //...state,
+//             r2.numOfIceCreams = r2.numOfIceCreams - 1;
+//         return r2;
+//         default: return state
+//     }
+// }
+
+
+const cakeReducer = (state = initialCakeState, action) => {
     switch(action.type){
         case BUY_CAKE: 
-        let r ={};
-        Object.assign(r, state);
-        r.numOfCakes = r.numOfCakes - 1;
+            let r ={};
+            Object.assign(r, state);
+            //...state,
+            r.numOfCakes = r.numOfCakes - 1;
         return r;
         default: return state
     }
 }
 
-// const reducer = (state = initialState, action) => {
-//     switch(action.type){
-//         case BUY_CAKE: return {
-//             //...state,
-//             numofCakes: state.numOfCakes - 1
-            
-//         }
-
-//         default: return state
-//     }
-// }
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+    switch(action.type){
+        case BUY_ICECREAM: 
+            let r2 ={};
+            Object.assign(r2, state);
+            //...state,
+            r2.numOfIceCreams = r2.numOfIceCreams - 1;
+        return r2;
+        default: return state
+    }
+}
 
 const store = createStore(reducer);
 console.log('initial state', store.getState());
@@ -45,6 +84,8 @@ const unsubscribe = store.subscribe(() => console.log('Updated state', store.get
 store.dispatch(buyCake());
 store.dispatch(buyCake());
 store.dispatch(buyCake());
+store.dispatch(buyIceCream());
+store.dispatch(buyIceCream());
 unsubscribe();
 
 
