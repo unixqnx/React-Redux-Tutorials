b4bc03c319e15beb6de8cca83fb234180a632cee
unixqnx <unixqnx@mail.ru>
2020-01-13 02:06:29 +0600

    React Redux Tutorials - 28 - Async Actions

--- React-Redux-Tutorials/react-redux-demo/src/redux/rootReducer.js
+++ React-Redux-Tutorials/react-redux-demo/src/redux/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import cakeReducer from "./cakes/cakeReducer";
 import iceCreamReducer from "./iceCream/iceCreamReduser";
+import userReducer from "./user/userReduser";
 
 const rootReducer = combineReducers({
     cake: cakeReducer,
-    iceCream: iceCreamReducer
+    iceCream: iceCreamReducer,
+    user: userReducer
 });
 
 export default rootReducer;
