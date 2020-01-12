37db79a2b7aaaba69f966f3720e3f9c2073481b7
unixqnx <unixqnx@mail.ru>
2020-01-12 23:24:57 +0600

    React Redux Tutorials - 25 - Action payload

--- React-Redux-Tutorials/react-redux-demo/src/App.js
+++ React-Redux-Tutorials/react-redux-demo/src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import CakeContainer from './components/CakeContainer';
 import HooksCakeContainer from './components/HooksCakeContainer';
 import IceCreamContainer from './components/IceCreamContainer';
+import NewCakeContainer from './components/NewCakeContainer';
+
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
@@ -14,6 +16,7 @@ function App() {
         <CakeContainer/>
         <HooksCakeContainer/>
         <IceCreamContainer/>
+        <NewCakeContainer/>        
       </div>
     </Provider>
   );
--- React-Redux-Tutorials/react-redux-demo/src/redux/cakes/cakeReducer.js
+++ React-Redux-Tutorials/react-redux-demo/src/redux/cakes/cakeReducer.js
@@ -9,13 +9,9 @@ const initialState = {
 const cakeReducer = (state = initialState, action) => {
     switch(action.type){
       case BUY_CAKE:
-        // let r = {};
-        // Object.assign(r, state);
-        // r.numOfCakes = r.numOfCakes - 1;
-        // return r;
       return {
         ...store,
-        numOfCakes: state.numOfCakes - 1
+        numOfCakes: state.numOfCakes - action.payload
       };
       default : return state;
     };
